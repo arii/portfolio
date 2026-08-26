167da80040fec738f41564d05f7ce379a7b03f9e
arii <arii@users.noreply.github.com>
2026-08-26 08:29:48 +0000

    deploy: preview for PR #255

---
+++ preview/pr-255/assets/swimlanesDiagram-VR7AAH4N-gJwh-HIc.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-Sd5SzLXz.js";import{_ as a}from"./index-DMXc6koO.js";import"./chunk-5VM5RSS4-Cj6TmTCQ.js";import"./chunk-XXDRQBXY-CazfB6qk.js";import"./chunk-POPQ4Y6H-D7ph8bD6.js";import"./chunk-F27PBJKO-fy_f55WE.js";import"./channel-CsFXBjOa.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
