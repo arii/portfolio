1b714319889252e7a5f9cf941aceb517793fb9e5
arii <arii@users.noreply.github.com>
2026-09-05 21:21:00 +0000

    deploy: preview for PR #312

---
+++ preview/pr-312/assets/Stack-DrjW0kTc.js
@@ -0,0 +1,6 @@
+import{c as r,j as f,B as h}from"./index-C9ihcH6z.js";/**
+ * @license lucide-react v1.31.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const p=[["path",{d:"M12 15V3",key:"m9g1x1"}],["path",{d:"M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4",key:"ih7n3h"}],["path",{d:"m7 10 5 5 5-5",key:"brsn70"}]],u=r("download",p),d=({direction:n="col",align:t,justify:s,wrap:a=!1,className:o="",...i})=>{const e=["flex",o];return n==="row"?e.push("flex-row"):e.push("flex-col"),t==="start"&&e.push("items-start"),t==="center"&&e.push("items-center"),t==="end"&&e.push("items-end"),t==="stretch"&&e.push("items-stretch"),t==="baseline"&&e.push("items-baseline"),s==="start"&&e.push("justify-start"),s==="center"&&e.push("justify-center"),s==="end"&&e.push("justify-end"),s==="between"&&e.push("justify-between"),s==="around"&&e.push("justify-around"),s==="evenly"&&e.push("justify-evenly"),a&&e.push("flex-wrap"),f.jsx(h,{className:e.filter(Boolean).join(" "),...i})};export{u as D,d as S};
---
+++ preview/pr-312/assets/swimlanesDiagram-VR7AAH4N-FSuVxWtg.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-1gh7Z0D0.js";import{_ as a}from"./ResearchDetailPage-hv62zJXW.js";import"./chunk-5VM5RSS4-PqACYAP5.js";import"./chunk-XXDRQBXY-DXVTH2Ie.js";import"./chunk-POPQ4Y6H-DjWNnVZ9.js";import"./chunk-F27PBJKO-DGYI0NA9.js";import"./channel-BHY_YZCx.js";import"./index-C9ihcH6z.js";import"./external-link-DNawGoxh.js";import"./asset-B-0Onugr.js";import"./Stack-DrjW0kTc.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,g=r({defaultLayout:"swimlane",styles:m});export{g as diagram};
